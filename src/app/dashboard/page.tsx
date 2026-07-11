d5af4867f13382e6cdcde715c28ac2ca453153a1
Mahliyo <gasandigital13@gmail.com>
2026-07-11 23:06:15 +0500

    dashboard style layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 export default function DashboardPage() {
   return (
-    <div className="space-y-6 style max-2xl: bg-amber-100 flex items-center justify-between">
+    <div className="space-y-6 style max-2xl: bg-amber-100 flex items-center flex-col">
       {/* Sarlavha */}
       <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
       
